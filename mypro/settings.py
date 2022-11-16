def690434d24135bd21ea8ae1129f3e329519624
Ketul Sheth <ketul.techcronus@gmail.com>
2022-11-16 12:41:24 +0530

    first commit

--- mypro/settings.py
+++ mypro/settings.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 
-from datetime import timedelta 
-from mypro import settings
+
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h01r8r)0kdo1iq@^my6-j0w_x^r^jk+^5mmn7rmirvv2h=b2@%'
 
 DEBUG = True
-"""Host Allowed"""
+
 ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
@@ -21,22 +20,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    #Rest_framework API,Swagger,JWTAuthentication Settings
-    'rest_framework',
-    'rest_framework_swagger',
-    'rest_framework_simplejwt',
-
-    #Social media settings
-    'django.contrib.sites',
-
-    # 'oauth_app',
-    'allauth',
-    'allauth.account',
-    'allauth.socialaccount',
-    'allauth.socialaccount.providers.google',
-    
-    #Application 
     'myapp'
 ]
 
@@ -110,64 +93,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'main'
 LOGOUT_REDIRECT_URL = 'login_view'
 
-#"""Sending Mail HOST User"""
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_FILE_PATH = str(BASE_DIR.joinpath('sent_emails'))
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'rishi.joshi9921@gmail.com'
-EMAIL_HOST_PASSWORD = "demo@1234ri"
-
-#"""Rest framework Libriries and Custome Settings"""
-REST_FRAMEWORK = {
-    'DEFAULT_SCHEMA_CLASS':'rest_framework.schemas.coreapi.AutoSchema' ,
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ],
-}
-
-#"""Json web token-JWT properties"""
-SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-    'ALGORITHM': 'HS256',
-    'SIGNING_KEY': settings.SECRET_KEY,
-    'LEEWAY': 0,
-
-    'AUTH_HEADER_TYPES': ('Bearer',),
-    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
-    'USER_ID_FIELD': 'id',
-    'USER_ID_CLAIM': 'user_id',
-    'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
-
-    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
-    'TOKEN_TYPE_CLAIM': 'token_type',
-
-    'JTI_CLAIM': 'jti',
-
-    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
-    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=10),
-    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
-}
-
-#"""Authhentication throughh social platform"""
-AUTHENTICATION_BACKENDS = [
-    'django.contrib.auth.backends.ModelBackend',
-    'allauth.account.auth_backends.AuthenticationBackend'
-]
-success_url = 'home' 
-#"""Set Google AS OAuth Provider"""
-SITE_ID = 3
-
-SOCIALACCOUNT_PROVIDERS = {
-    'google': {
-        'SCOPE': [
-            'profile',
-            'email',
-        ],
-        'AUTH_PARAMS': {
-            'access_type': 'offline',
-        }
-    }
-}
